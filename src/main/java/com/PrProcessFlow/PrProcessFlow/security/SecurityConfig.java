3161c21f34dfd67720b7ab770ac9f377d00c3541
Mohd Anas <mohdanashasmi@gmail.com>
2025-12-09 12:45:59 +0530

    Updated code

--- src/main/java/com/PrProcessFlow/PrProcessFlow/security/SecurityConfig.java
+++ src/main/java/com/PrProcessFlow/PrProcessFlow/security/SecurityConfig.java
@@ -49,8 +49,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("https://zepto-pr-process.netlify.app/"));
-        configuration.setAllowedOrigins(List.of("http://localhost:5173/"));
+        configuration.setAllowedOrigins(List.of("https://zepto-pr-process.netlify.app/"));
+//        configuration.setAllowedOrigins(List.of("http://localhost:5173/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // if you ever send cookies
